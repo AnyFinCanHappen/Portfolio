972cbb9cf3737843ac4a42f3b4f07a0c457bd1b1
Sidney Tan <sidneyjt@uci.edu>
2020-08-13 18:44:50 -0700

    Added bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
 import ReactPlayer from 'react-player'
-import {Link, animateScroll as scroll} from "react-scroll"
+import {Link, animateScroll as scroll} from "react-scroll";
 import './App.css';
-import NavBar from './NavBar'
-import logo from "./css/self_image.jpg"
+import NavBar from './NavBar';
+import logo from "./css/self_image.jpg";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import background_image from "./css/background_mono.jpg";
+import Image from "react-bootstrap/Image";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 class App extends Component
@@ -29,78 +36,97 @@ class App extends Component
   </Link>);
   }
   
+  /*
+            <div className = "background_image">
+            <NavBar scrollToFunc = {this.scrollToFunc}> </NavBar>
+            <div className = "intro">
+                Sidney Tan
+                <p>
+                  Full Stack Developer
+                </p>
+                <this.scrollToFunc section = "bio"/>
+            </div>
+          </div>
+  */
+
   render()
   {
     return(
       <div className = "App">
-        <div className = "background_image">
-          <NavBar> </NavBar>
-          <div className = "intro">
-              Sidney Tan
-              <p>
-                Full Stack Developer
-              </p>
-              <this.scrollToFunc section = "bio"/>
+        <NavBar > </NavBar>
+        <Container fluid>
+          <Row >
+            <Col className = "px-0">
+            <Image style = {{backgroundSize: "auto"}} src = {background_image} fluid></Image>
+
+            <div className = "intro">
+                Sidney Tan
+                <br></br>
+                  Full Stack Developer
+                <br></br>
+                <this.scrollToFunc section = "bio"/>
+            </div>
+            </Col>
+          </Row>
+          <div className = "bio_section" id = "bio">
+            <p className = "title">
+              Bio
+            </p>
+            <p className = "bio_paragraph">
+              Hello! I am a computer science major from University California Irvine. My ideal postion is to be full stack devloper. Languages I know
+              are Java, React, C++, Python, mySQL, and many more.
+              <br></br><br></br>
+              Other hobbies include playing video games (Valorant, League of Legends, Escape From Tarkov, ...) and guitar.
+            </p>
+            <this.scrollToFunc section = "project_title"/>
           </div>
-        </div>
-        <div className = "bio_section" id = "bio">
-          <p className = "title">
-            Bio
-          </p>
-          <p className = "bio_paragraph">
-            Hello! I am a computer science major from University California Irvine. My ideal postion is to be full stack devloper. Languages I know
-            are Java, React, C++, Python, mySQL, and many more.
+          <div className = "projects_section" id = "projects">
+            <p className = "title" id = "project_title">
+              Projects
+            </p>
+            <p className = "fabflix_title">
+              Fabflix
+            </p>
+            <ReactPlayer url = "https://www.youtube.com/watch?v=mP02Bd30sa8" controls = "true" className = "fabflix_player">
+            </ReactPlayer>
+            <p className = "fabflix_description">
+              Web application that was built in one of my project course at UCI.
+              <br></br>
+              Front End:
+              React, Node.js, AJAX
+              <br></br>
+              Back End:
+              Java
+              <br></br>
+              Databases:
+              MySQL, JDBC
+              <br></br>
+              Other tools:
+              GitHub, Gradle, Postman
+            </p>
+            <p className = "github_title">
+              GitHub
+            </p>
+            <p className = "github_description">
+              Link to my <a href="https://github.com/AnyFinCanHappen" target="_blank" rel="noopener noreferrer">GitHub</a>
+            </p>
+            <this.scrollToFunc section = "contact"/>
+          </div>
+          <div className = "contact_section" id = "contact">
+            <p className = "title">
+                Contact Me
+            </p>
+            <p className = "github_title">
+              <a href="https://github.com/AnyFinCanHappen" target="_blank" rel="noopener noreferrer">Linkedin</a>
+            </p>
+            <p className = "github_title">
+              sidneyjt@uci.edu
+            </p>
+            <img src = {logo} alt ="self" className = "self_image" />
             <br></br><br></br>
-            Other hobbies include playing video games (Valorant, League of Legends, Escape From Tarkov, ...) and guitar.
-          </p>
-          <this.scrollToFunc section = "project_title"/>
-        </div>
-        <div className = "projects_section" id = "projects">
-          <p className = "title" id = "project_title">
-            Projects
-          </p>
-          <p className = "fabflix_title">
-            Fabflix
-          </p>
-          <ReactPlayer url = "https://www.youtube.com/watch?v=mP02Bd30sa8" controls = "true" className = "fabflix_player">
-          </ReactPlayer>
-          <p className = "fabflix_description">
-            Web application that was built in one of my project course at UCI.
-            <br></br>
-            Front End:
-            React, Node.js, AJAX
-            <br></br>
-            Back End:
-            Java
-            <br></br>
-            Databases:
-            MySQL, JDBC
-            <br></br>
-            Other tools:
-            GitHub, Gradle, Postman
-          </p>
-          <p className = "github_title">
-            GitHub
-          </p>
-          <p className = "github_description">
-            Link to my <a href="https://github.com/AnyFinCanHappen" target="_blank" rel="noopener noreferrer">GitHub</a>
-          </p>
-          <this.scrollToFunc section = "contact"/>
-        </div>
-        <div className = "contact_section" id = "contact">
-          <p className = "title">
-              Contact Me
-          </p>
-          <p className = "github_title">
-            <a href="https://github.com/AnyFinCanHappen" target="_blank" rel="noopener noreferrer">Linkedin</a>
-          </p>
-          <p className = "github_title">
-            sidneyjt@uci.edu
-          </p>
-          <img src = {logo} alt ="self" className = "self_image" />
-          <br></br><br></br>
-          <a onClick = {this.scrollToTop} href="/#" className = "scroll-up"> </a>
-        </div>
+            <a onClick = {this.scrollToTop} href="/#" className = "scroll-up"> </a>
+          </div>
+        </Container>
       </div>
     );
   }
