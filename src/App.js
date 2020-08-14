495ff5baea9b3dda581bf2d81867b765fdb831e4
Sidney Tan <sidneyjt@uci.edu>
2020-08-14 16:10:49 -0700

    Page is more mobile friendly.

--- src/App.js
+++ src/App.js
@@ -88,65 +88,84 @@ class App extends Component
             </div>
             </Col>
           </Row>
-          <div className id = "bio">
-            <p className = "title">
-              Bio
-            </p>
-            <p className = "bio_paragraph">
-              Hello! I am a computer science major from University California Irvine. My ideal postion is to be full stack devloper. Languages I know
-              are Java, React, C++, Python, mySQL, and many more.
-              <br></br><br></br>
-              Other hobbies include playing video games (Valorant, League of Legends, Escape From Tarkov, ...) and guitar.
-            </p>
-            <this.scrollToFunc section = "project_title"/>
-          </div>
-          <div  id = "projects">
-            <p className = "title" id = "project_title">
-              Projects
-            </p>
-            <p className = "fabflix_title">
-              Fabflix
-            </p>
-            <ReactPlayer url = "https://www.youtube.com/watch?v=mP02Bd30sa8" controls = "true" className = "fabflix_player" width = {videoPlayerWidth} height = {videoPlayerHeight}>
-            </ReactPlayer>
-            <p className = "fabflix_description">
-              Web application that was built in one of my project course at UCI.
-              <br></br>
-              Front End:
-              React, Node.js, AJAX
-              <br></br>
-              Back End:
-              Java
-              <br></br>
-              Databases:
-              MySQL, JDBC
-              <br></br>
-              Other tools:
-              GitHub, Gradle, Postman
-            </p>
-            <p className = "github_title">
-              GitHub
-            </p>
-            <p className = "github_description">
-              Link to my <a href="https://github.com/AnyFinCanHappen" target="_blank" rel="noopener noreferrer">GitHub</a>
-            </p>
-            <this.scrollToFunc section = "contact"/>
-          </div>
-          <div  id = "contact">
-            <p className = "title">
-                Contact Me
-            </p>
-            <p className = "github_title">
-              <a href="https://www.linkedin.com/in/sidney-tan-60a6b1192" target="_blank" rel="noopener noreferrer">Linkedin</a>
-            </p>
-            <p className = "github_title">
-              sidneyjt@uci.edu
-            </p>
-            <img src = {logo} alt ="self" className = "self_image" />
-            <br></br><br></br>
-            <a onClick = {this.scrollToTop} href="/#" className = "scroll-up"> </a>
-            <br></br><br></br>
-          </div>
+          <Row>
+            <Col></Col>
+            <Col md = {6} xs = {10}>
+              <div id = "bio">
+                <p className = "title">
+                  Bio
+                </p>
+                <p className = "bio_paragraph">
+                  Hello! I am a computer science major from University California Irvine. My ideal postion is to be full stack devloper. Languages I know
+                  are Java, React, C++, Python, mySQL, and many more.
+                  <br></br><br></br>
+                  Other hobbies include playing video games (Valorant, League of Legends, Escape From Tarkov, ...) and guitar.
+                </p>
+                <this.scrollToFunc section = "project_title"/>
+              </div>
+            </Col>
+              <Col></Col>
+          </Row>
+          <Row>
+            <Col></Col>
+            <Col md = {6} xs = {10}>
+              <div  id = "projects">
+                <p className = "title" id = "project_title">
+                  Projects
+                </p>
+                <p className = "fabflix_title">
+                  Fabflix
+                </p>
+                <ReactPlayer url = "https://www.youtube.com/watch?v=mP02Bd30sa8" controls = "true" className = "fabflix_player" width = {videoPlayerWidth} height = {videoPlayerHeight}>
+                </ReactPlayer>
+                <br></br>
+                <p className = "fabflix_description">
+                  CRUD web app that allows users to buy/search movies.
+                  <br></br>
+                  <b>Front End</b>:
+                  React, Node.js
+                  <br></br>
+                  <b>Back End</b>:
+                  Java
+                  <br></br>
+                  <b>Databases</b>:
+                  MySQL, JDBC
+                  <br></br>
+                  Other tools:
+                  GitHub, Gradle, Postman
+                </p>
+                <p className = "github_title">
+                  GitHub
+                </p>
+                <p className = "github_description">
+                  Link to my <a href="https://github.com/AnyFinCanHappen" target="_blank" rel="noopener noreferrer">GitHub</a>
+                </p>    
+                <this.scrollToFunc section = "contact"/>
+              </div>
+            </Col>
+            <Col></Col>
+          </Row>
+          <Row>
+            <Col></Col>
+            <Col md = {6} xs = {10}>
+              <div  id = "contact">
+                <p className = "title">
+                    Contact Me
+                </p>
+                <p className = "github_title">
+                  <a href="https://www.linkedin.com/in/sidney-tan-60a6b1192" target="_blank" rel="noopener noreferrer">Linkedin</a>
+                </p>
+                <p className = "github_title">
+                  sidneyjt@uci.edu
+                </p>
+                <img src = {logo} alt ="self" className = "self_image" />
+                <br></br><br></br>
+                <a onClick = {this.scrollToTop} href="/#" className = "scroll-up"> </a>
+                <br></br><br></br>
+              </div>
+            </Col>
+            <Col></Col>
+          </Row>
         </Container>
       </div>
     );
